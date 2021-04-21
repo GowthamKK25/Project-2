b755ce53513fa5ea04f563504bbcf28fd4c236d9
Maria Barrera <mbarrera_usa@yahoo.com>
2021-04-20 19:06:49 -0700

    update files

---
+++ js/d3.js
@@ -0,0 +1,218 @@
+
+
+var svgWidth = 1000;
+var svgHeight = 750;
+
+var margin = {
+  top: 30,
+  right: -30,
+  bottom: 85,
+  left: 200
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "profit";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(stockData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(stockData, d => d[chosenXAxis]) * 0.8,
+      d3.max(stockData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+  var label;
+
+  if (chosenXAxis === "profit") {
+    label = "profit(in millions) :";
+  }
+  else {
+    label = "Rank:";
+  }
+
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([100, -60])
+    .html(function(d) {
+      return (`${d.company}<br>${label} ${d[chosenXAxis]}`);
+    });
+
+  circlesGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+  })
+    // onmouseout event
+    .on("mouseout", function(data) {
+      toolTip.hide(data);
+    });
+
+  return circlesGroup;
+}
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("fortune500-2019.csv").then(function(stockData, err) {
+  if (err) throw err;
+
+  // parse data
+  stockData.forEach(function(data) {
+    data.profit = +data.profit;
+    data.rank = +data.rank; 
+    data.revenue = +data.revenue;
+  });
+
+  // xLinearScale function above csv import
+  var xLinearScale = xScale(stockData, chosenXAxis);
+
+  // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(stockData, d => d.profit)])
+    .range([height, 0]);
+
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // append initial circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(stockData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d.revenue))
+    .attr("r",2.5)
+    .attr("fill", "black")
+    .attr("opacity", ".5");
+
+  // Create group for two x-axis labels
+  var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+  var profitLengthLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("value", "profit") // value to grab for event listener
+    .classed("active", true)
+    .text("Profit in millions");
+
+  var rankLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 40)
+    .attr("value", "rank") // value to grab for event listener
+    .classed("inactive", true)
+    .text("Rank of the company");
+
+  // append y axis
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .classed("axis-text", true)
+    .text("Revenue in millions");
+
+  // updateToolTip function above csv import
+  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+  // x axis labels event listener
+  labelsGroup.selectAll("text")
+    .on("click", function() {
+      // get value of selection
+      var value = d3.select(this).attr("value");
+      if (value !== chosenXAxis) {
+
+        // replaces chosenXAxis with value
+        chosenXAxis = value;
+
+        // console.log(chosenXAxis)
+
+        // functions here found above csv import
+        // updates x scale for new data
+        xLinearScale = xScale(stockData, chosenXAxis);
+
+        // updates x axis with transition
+        xAxis = renderAxes(xLinearScale, xAxis);
+
+        // updates circles with new x values
+        circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+
+        // updates tooltips with new info
+        circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+        // changes classes to change bold text
+        if (chosenXAxis === "profit") {
+            rankLabel
+            .classed("active", true)
+            .classed("inactive", false);
+            profitLengthLabel
+            .classed("active", false)
+            .classed("inactive", true);
+        }
+        else {
+          rankLabel
+            .classed("active", false)
+            .classed("inactive", true);
+          profitLengthLabel
+            .classed("active", true)
+            .classed("inactive", false);
+        }
+      }
+    });
+}).catch(function(error) {
+  console.log(error);
+}); 
---
+++ js/geomap.js
@@ -0,0 +1,136 @@
+// <!--#######################################################################  
+//     Author:  Maria Barrera
+//     Date: 04/17/2021
+//     GeoMap page to display data visualization of housing prices 
+//     in Sacramento, CA using an Excel csv input file.
+//     ########################################################################-->
+
+// Coding references:
+//    1) R17 - Day 1 - Activity 7 -- replace points with house prices
+//    2) R17 - Day 2 - Activity 4 -- to add legend
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Note: MB token / API key -- to be used for this web page only
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const API_KEY = "pk.eyJ1IjoibWJhcnJlcmEwMDciLCJhIjoiY2tuMXhkYmZmMTIydTJ2bWx1aGt5bXQ1aSJ9.kglY5P7hJ14rXkgABJIFkg";
+
+// Create a map object
+var myMap = L.map("map", {
+    center: [38.575764, -121.47885],  // use Sacramento, CA as starting point
+    zoom: 10.5                        // zoom 10x so markers can be seen
+  });
+  
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+// ---------------------------------------------------------------------------
+// Use d3 to open and read excel csv file
+// Sacramentorealestatetransactions.csv -- full data
+// Sacramentorealestatetransactions_sub1.csv -- subset (removed 0 sq ft data)
+// ---------------------------------------------------------------------------
+  d3.csv("../Resources/Sacramentorealestatetransactions_sub1.csv").then(data =>{
+    renderHouses(data)
+    console.log(data)
+    // var street = data.street;
+    // var latitude = data.latitude;
+    // var longitude = data.longitude;
+  })
+
+  
+function renderHouses(houses){
+ // Loop through the houses array and create one marker for each house & 
+ // add color coding based on price of the house
+  for (var i = 0; i <houses.length; i++) {
+  
+    // Conditionals for house prices
+    var color = "";
+    if (houses[i].price > 600000) {
+      color = "blue";
+     }
+    else if (houses[i].price > 500000) {
+     color = "green";
+    }
+    else if (houses[i].price > 400000) {
+      color = "aquamarine";
+    }
+    else if (houses[i].price > 300000) {
+      color = "orange";
+    }
+    else if (houses[i].price > 200000) {
+      color = "violet";
+    }
+    else {
+      color = "red";
+    }
+  
+    // Add circles to the map and display the street & price
+    L.circle([houses[i].latitude, houses[i].longitude], {
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: color,
+      radius: houses[i].price / 500   // Adjust radius of circle by dividing the price
+    }).bindPopup(
+        "<h4>" + houses[i].street + 
+        "</h4> <hr> <h5>Price: " + houses[i].price + "</h5>" +
+        "<h5>Sq_ft: " + houses[i].sq__ft + "</h5>" +
+        "<h5>BR: " + houses[i].beds + 
+        "   BA: " + houses[i].baths + "</h5>" +
+        "<h5>Date: " + houses[i].sale_date + "</h5>" 
+        ).addTo(myMap);
+  }  // end of for loop for function
+
+// ``````````````````````````````````````
+// insert legend code here
+// ``````````````````````````````````````
+//-----------------------------------------------------------------------------------------
+// ** since legend is not run as a function, put code as part of the Create Map Function
+// // *************************************************************************************
+// // Map LEGEND code section
+// // Reference -- legend code from student activity 17-Day2-Activities4
+// // *************************************************************************************
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+var div = L.DomUtil.create("div", "info legend");
+
+var limits = [0, 1, 2, 3, 4, 5];
+var colors = ["red", "violet", "orange", "aquamarine", "green", "blue"];
+
+var labels = [];
+
+// Add min & max
+var legendInfo = "<h4>Sacramento Home Prices " + "</h4>" +
+  "</h5> <200k, <300k, <400k, <500k ... >600k)" + "</h5>" + "<h5>"
+  "<div class=\"labels\">" +
+    // "<div class=\"min\">" + limits[0] + "</div>" +
+    // "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  "</div>";
+
+div.innerHTML = legendInfo;
+
+limits.forEach(function(limit, index) {
+  labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+});
+
+// div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+div.innerHTML += labels.join("");
+return div;
+};
+
+// Adding legend to the map
+legend.addTo(myMap);
+
+//----------- end of legend code ----------
+
+
+}  // end of renderHouses function
+
+
+
+
+
